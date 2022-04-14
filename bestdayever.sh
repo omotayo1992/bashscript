b2b63d8dceefee0e2e8951b0550b68eeca2e35e8
EC2 Default User <ec2-user@ip-172-31-84-4.ec2.internal>
2022-04-14 02:37:44 +0000

    first commit

---
+++ bestdayever.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+echo "What is your name"
+
+read name
+
+
+echo "Goodnight $name!!!"
+
+sleep 2
+
+echo "you are looking great today $name"
+
+sleep 2
+
+echo "You have the best hair i have ever seen $name"
+
+
+
+sudo yum update -y
+
+
+sudo yum install httpd -y
+
+sudo systemctl start httpd
+
+echo "We are the best bash script writters in the world" > /var/www/html/index.html
+
+cd /home/ec2-user
+
+mkdir patricia solomon david Jesus Nelly Niome Aida Tolu .
+
+cd solomon
+
+touch abimbola.txt ajetunmobi.txt lookkman.py
+
+
+echo "Hello this is amazing" > ./olabiyi.txt
+
+echo "Job well done"
+
+echo "you all are genius" > ./tit.txt
+
